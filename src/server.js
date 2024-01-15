d429767278e7d7c2390ae9fd5ac63de2f8ea6885
Ryan <ryan02ff@gmail.com>
2024-01-15 10:33:12 -0300

    first configs on server.js import express, routes and listen on port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
-const express = require('express')
+const express = require("express");
+const app = express();
 
-const app = express()
+app.use(express.json()); //Para a api saber qual o padrão vamos utilizar para enviar infos que no caso é o json.
 
-const PORT = 3333
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+const routes = require('./routes')
+
+app.use(routes)
+
+const PORT = 3333;
+app.listen(PORT, () =>
+  console.log(`Server is running at http://localhost:${PORT}`)
+);
+ 
