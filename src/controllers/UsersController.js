589fb76d5675849a10442a961ce0cecc213075d3
Ryan <ryan02ff@gmail.com>
2024-01-16 17:43:10 -0300

    verification created if the email provided already exists, encrypted password and registered user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,10 +1,26 @@
-class UsersController {
-  async create(request, response) {
-    const { name, email, password } = request.body;
+const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
+const { hash } = require("bcryptjs") 
 
-    response.status(201).json({ name, email, password });
+class UsersController {
+  async create(req, res) {
+    const { name, email, password } = req.body;
+
+    const database = await sqliteConnection()
+    const userExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    
+    if(userExists) {
+      throw new AppError("Este endereço de email já está cadastrado.", 409)
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+
+
+    res.status(201).json("Usuário cadastrado!");
   }
-  
+
 }
 
 module.exports = UsersController
