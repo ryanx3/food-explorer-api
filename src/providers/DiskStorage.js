ee68ba1f291e9eaf01e57633026cd6cf7d6bc020
Ryan <ryangabriel_cap@hotmail.com>
2024-02-01 12:19:21 -0300

    create diskStorage for save and delete files

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const uploadConfig = require("../configs/upload");
+
+class diskStorage {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.TEMP_FOLDERS, file),
+      path.resolve(uploadConfig.UPLOAD_FOLDERS, file)
+    );
+
+    return file;
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOAD_FOLDERS, file);
+
+    try {
+        await fs.promises.stat(filePath)
+    } catch (error) {
+        return
+    }
+    await fs.promises.unlink(filePath)
+  }
+}
+
+module.exports = diskStorage;
