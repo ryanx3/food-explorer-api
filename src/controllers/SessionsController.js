7c868287ccccfd9a12bb2fb99176f7000b615123
Ryan <ryangabriel_cap@hotmail.com>
2024-02-02 11:56:26 -0300

    "

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -11,13 +11,13 @@ class SessionsController {
     const user = await knex("users").where({ email }).first();
 
     if (!user) {
-      throw new AppError("Email e/ou senha incorreta.", 401);
+      throw new AppError("Email e/ou senha incorretos.", 401);
     }
 
     const passwordMatched = await compare(password, user.password);
     
     if (!passwordMatched) {
-      throw new AppError("Email e/ou senha incorreta.", 401);
+      throw new AppError("Email e/ou senha incorretos.", 401);
     }
 
     const { expiresIn, secret } = authConfigs.jwt
