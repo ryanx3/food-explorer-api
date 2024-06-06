63bf4201e3b2ebae8d6fdae4249bccab26e620e0
Ryan <ryangabriel_cap@hotmail.com>
2024-06-07 00:16:21 +0100

    refactory: Atualizando formato de favoritar e desfavoritar pratos

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -3,80 +3,81 @@ const knex = require("../database/knex");
 
 class FavoritesController {
   async create(req, res) {
-    const user_id = req.user.id;
     const { dish_id } = req.params;
+    const user_id = req.user.id;
 
-    const [id] = await knex("favoriteDishes").insert({
-      dish_id,
-      user_id,
-    });
-    if (!id) {
-      throw new AppError("Não foi possível favoritar o prato", 401);
+    try {
+      const [id] = await knex("favorites").insert({
+        dish_id,
+        user_id,
+      });
+      if (!id) {
+        throw new AppError("Não foi possível favoritar o prato", 401);
+      }
+
+      return res.json("Prato favoritado com sucesso!");
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).json({ error: "Erro interno do servidor" });
     }
-
-    return res.json("Prato favoritado com sucesso");
   }
 
   async delete(req, res) {
-      const user_id = req.user.id
-    const { dish_id } = req.params
+    const { dish_id } = req.params;
+    const user_id = req.user.id;
 
-    const usersFavoriteDishes = await knex("favoriteDishes").where({ user_id })
+    try {
+      const deleted = await knex("favorites").where({ dish_id, user_id }).del();
 
-    let dishIdFavorites
-
-    for (let i = 0; i < usersFavoriteDishes.length; i++) {
-      if (usersFavoriteDishes[i].dish_id == dish_id) {
-        dishIdFavorites = usersFavoriteDishes[i].id
+      if (!deleted) {
+        throw new AppError("Prato favorito não encontrado para exclusão", 404);
       }
-    }
 
-    if (dishIdFavorites) {
-      await knex("favoriteDishes").where({ id: dishIdFavorites }).delete()
-    } else {
-      throw new AppError("Não foi possível localizar o prato favorito")
+      return res.json({ message: "Prato desfavoritado com sucesso!" });
+    } catch (error) {
+      console.error(error.message);
+      return res
+        .status(error.status || 500)
+        .json({ error: "Erro interno do servidor" });
     }
-
-    return res.json({ message: "Prato favorito deletado com sucesso" })
   }
 
   async index(req, res) {
-    const user_id = req.user.id
-    const { role } = req.user
+    const user_id = req.user.id;
 
     if (role === "customer") {
-      const usersFavoriteDishes = await knex("favoriteDishes").where({
+      const usersFavoriteDishes = await knex("favorites").where({
         user_id,
-      })
+      });
 
       if (usersFavoriteDishes) {
-        return res.json(usersFavoriteDishes)
+        return res.json(usersFavoriteDishes);
       } else {
-        throw new AppError("Não foi possível localizar os pratos favoritos")
+        throw new AppError("Não foi possível localizar os pratos favoritos");
       }
     } else {
-      const dishesFavorites = await knex("favoriteDishes").orderBy("dish_id")
+      const dishesFavorites = await knex("favorites").orderBy("dish_id");
 
-      const count = {}
+      const count = {};
       dishesFavorites.forEach((object) => {
-        const dish_id = object.dish_id
+        const dish_id = object.dish_id;
 
         if (count[dish_id]) {
-          count[dish_id].amount++
+          count[dish_id].amount++;
         } else {
           count[dish_id] = {
             ...object,
             amount: 1,
-          }
+          };
         }
-      })
+      });
 
-      const newArray = Object.values(count).map((item) => item)
+      const newArray = Object.values(count).map((item) => item);
 
       if (!newArray) {
-        throw new AppError("Não foi possível localizar os pratos favoritos")
+        throw new AppError("Não foi possível localizar os pratos favoritos");
       }
-      return res.json(newArray)
+      return res.json(newArray);
     }
   }
 }
