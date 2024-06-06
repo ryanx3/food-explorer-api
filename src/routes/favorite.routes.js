f56911aa718d44c5bb2dd6f361853da6c60deb9a
Ryan <ryangabriel_cap@hotmail.com>
2024-06-06 21:51:12 +0100

    feat: Ajustando rotas para usuário cadastrar prato favorito

--- src/routes/favorite.routes.js
+++ src/routes/favorite.routes.js
@@ -1,12 +1,14 @@
-const Router = require("express");
+const { Router } = require("express");
 const favoritesRouter = Router();
+
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const FavoritesController = require("../controllers/FavoritesController");
 const favoritesController = new FavoritesController();
 
-favoritesRouter.use(ensureAuthenticated);
 
-favoritesRouter.post("/:dish_id", favoritesController.create);
+favoritesRouter.post("/:dish_id", ensureAuthenticated, favoritesController.create);
 favoritesRouter.delete("/:dish_id", favoritesController.delete);
 favoritesRouter.get("/", favoritesController.index);
+
+module.exports = favoritesRouter
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
-const { Router } = require("express")
-const routes = Router()
-const usersRouter = require("./users.routes")
-const dishesRouter = require("./dishes.routes")
-const sessionsRouter = require("./sessions.routes")
-const ingredientsRouter = require("./ingredients.routes")
+const { Router } = require("express");
+const routes = Router();
 
-routes.use("/users", usersRouter)
-routes.use("/dishes", dishesRouter)
-routes.use("/ingredients", ingredientsRouter)
-routes.use("/sessions", sessionsRouter)
+const usersRouter = require("./users.routes");
+const dishesRouter = require("./dishes.routes");
+const sessionsRouter = require("./sessions.routes");
+const ingredientsRouter = require("./ingredients.routes");
+const favoritesRouter = require("./favorite.routes");
+const userAdressRouter = require("./userAdress.routes");
 
-module.exports = routes
+routes.use("/users", usersRouter);
+routes.use("/dishes", dishesRouter);
+routes.use("/ingredients", ingredientsRouter);
+routes.use("/favorite", favoritesRouter);
+routes.use("/user-adress", userAdressRouter);
+routes.use("/sessions", sessionsRouter);
+
+module.exports = routes;
