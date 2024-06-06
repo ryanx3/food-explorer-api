542a1975eaba5b482301e1fb3cf6a4581bc31583
Ryan <ryangabriel_cap@hotmail.com>
2024-06-07 00:17:42 +0100

    feat: Corrigindo rotas dos favorites

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,13 @@ const usersRouter = require("./users.routes");
 const dishesRouter = require("./dishes.routes");
 const sessionsRouter = require("./sessions.routes");
 const ingredientsRouter = require("./ingredients.routes");
-const favoritesRouter = require("./favorite.routes");
+const favoritesRouter = require("./favorites.routes");
 const userAdressRouter = require("./userAdress.routes");
 
 routes.use("/users", usersRouter);
 routes.use("/dishes", dishesRouter);
 routes.use("/ingredients", ingredientsRouter);
-routes.use("/favorite", favoritesRouter);
+routes.use("/favorites", favoritesRouter);
 routes.use("/user-adress", userAdressRouter);
 routes.use("/sessions", sessionsRouter);
 
