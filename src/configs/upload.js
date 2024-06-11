25aa7be1620c08d459b6756ab92a86e2a4529792
Ryan <ryangabriel_cap@hotmail.com>
2024-06-11 20:52:44 +0100

    refactory: Organizando códigos

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,25 +1,24 @@
-const path = require("path")
-const multer = require("multer")
-const crypto = require("crypto")
+const path = require("path");
+const multer = require("multer");
+const crypto = require("crypto");
 
-const TEMP_FOLDERS = path.resolve(__dirname, "..", "..", "tmp")
-const UPLOAD_FOLDERS = path.resolve(TEMP_FOLDERS, "uploads")
+const TEMP_FOLDERS = path.resolve(__dirname, "..", "..", "tmp");
+const UPLOAD_FOLDERS = path.resolve(TEMP_FOLDERS, "uploads");
 
 const MULTER = {
-    storage: multer.diskStorage({
-        destination: TEMP_FOLDERS,
-        filename(req, file, callback) {
-            const hashFile = crypto.randomBytes(10).toString("hex")
-            const filename = `${hashFile}-${file.originalname}`
+  storage: multer.diskStorage({
+    destination: TEMP_FOLDERS,
+    filename(req, file, callback) {
+      const hashFile = crypto.randomBytes(10).toString("hex");
+      const filename = `${hashFile}-${file.originalname}`;
 
-            return callback(null, filename)
-        }
-    })
-}
-
+      return callback(null, filename);
+    },
+  }),
+};
 
 module.exports = {
-    TEMP_FOLDERS,
-    UPLOAD_FOLDERS,
-    MULTER
-}
+  TEMP_FOLDERS,
+  UPLOAD_FOLDERS,
+  MULTER,
+};
